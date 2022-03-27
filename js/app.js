0855cbfd39edf5d7e795fe6d2352200db183542b
Bmcuevas <97862094+Bmcuevas@users.noreply.github.com>
2022-03-27 14:38:28 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,83 @@
+const array = ["¿Que es lo que te gusta hacer para relajar?","¿Alguna cosa chiquita que te alegre el dia?","¿A que le tenes miedo?", "Si hoy te ofrecieran irte de viaje ¿ A dónde te irías?","¿Qué es lo que más te gusta de vos?",
+"Suponiendo que la reencarnación existe, ¿en que o en quién te gustaría reencarnar?","¿Cuándo te parece que sabes que estás enamorado de alguien?",
+"¿Perdonaste o perdonarías una infidelidad?","¿Hay algo que harías diferente o que cambiarías de tu pasado?",
+"¿Que es lo que más te gusta de tu cuerpo?","¿Cuál es tu ídolo?","¿Algo que cambiarias del mundo?","¿Sos feliz?","¿Cuál es tu cábala?¿Para qué?",
+"¿En que momentos o situaciones sentís que sos amado?","¿Que tomas cuando salis?","¿Cuál es tu día favorito del año?",
+"¿Qué es lo que te enamora de una persona?","¿Qué estás pensando en comprarte?","¿Elegirías ser inmortal?¿Por qué?",
+"¿Cómo te imaginás en el futuro?","¿Messi o Maradona?","¿Cómo superaste a tu primer amor?","¿Sabes por qué te pusieron ese nombre?",
+"¿4 cosas que ames","¿Cuál consideras que fue el mejor momento de tu vida?","¿Cuál es tu lugar favorito?",
+"¿Alguien que ames?","¿Cuestionas lo que lees en un libro?¿Por qué?","¿Te tatuarías? ¿Qué?","¿Cuál es tu numero de la suerte?",
+"¿Pelo largo o pelo corto?","¿Amor o dinero?","¿Qué idioma te gustaría hablar?","¿Te gusta tu nombre?","¿Sabes qué significa tu nombre?",
+"¿Tocás algun instrumento? ¿Cuál? Si no, ¿cual te gustaría tocar?","¿Cuál fue tu mejor golpe de suerte?","¿Te caen bien tus vecinos?",
+"¿Animal favorito?","¿Te irías de viaje a los mejores lugares con alguien a quién no soportes?","¿Cambiarías tu vida por la de otra persona?",
+"¿Qué es lo mejor que te haya enseñado una persona?","¿Qué define que algo esté bien o mal?","¿Qué es lo que no te puede faltar en la heladera?",
+"¿Cuándo fue la última vez que fuiste gentil con el corazón de un extraño?","¿Estás listo para un verano de amor?","¿Cuál es tu pregunta favorita?",
+"¿Que harías si no tuvieras miedo?","¿Sonido favorito?","¿Cuándo te sentís insegura?","¿Cómo definirías la belleza? ¿Crees que sos lindo/a?",
+"¿Qué cosa de tu vida actual, tu versión adolescente no podría creer?","¿Rubios o morochos?","¿Qué es un amigo para vos?",
+"¿3 palabras que te gusten?","¿Qué harías mañana si se acabaría el mundo pasado mañana?","¿Te consideras una persona amada?",
+"¿Alguna vez intentaste cambiar a una persona?¿y pudiste?","¿Te gusta la afrenalina? (Tirarte de un paracaídas, bungee jumping, etc)",
+"¿Te hago acordar a alguien?¿A quién?","¿Que es el amor propio para vos?","¿Qué es la felicidad para vos?",
+"¿Que superpoder elegirías tener?","¿Canción favorita?",
+"¿A quién elegirías para acompañarte en todas las aventuras que se te presenten por el resto de tu vida?","¿Un tema del que te guste hablar?",
+"1/4 de helado ¿Qué gustos elegís?","¿Crees en la amistad entre el hombre y la mujer?",
+"¿Pensáis que hay algo después de la muerte? ¿Como lo describirías?","¿Algo que harías si fuese el último día de tu vida?",
+"Si tenes plata extra, ¿en qué la gastas?","¿Crees en algún Dios?¿cómo lo definirías?","¿Película favorita?",
+"¿Cómo se llama tu papá? ¿me decís una cosa acerca de él?","¿Te gusta tu nombre?","¿Te gusta tu cumpleaños?",
+"¿Extrañas a alguien en tu vida hoy? Si quisieras ¿podrías invitarlo/la a juntarse?","¿Te enamoraste alguna vez?",
+"Hoy ¿estás enamorado de alguien o te gusta alguien?","¿Algún dato curioso?","¿Sentís que la sociedad te presiona en algunas cuestiones?",
+"Si pudieras cambiar una sola cosa de tu cuerpo, ¿qué cambiarías?","¿Verano o invierno?","¿Fuiste infiel alguna vez?",
+"Si te gusta alguien¿Le hablas vos o que la otra persona te hable?","¿Libros o series?","¿Te da miedo llegar a alguna determinada edad? ¿Cuál?",
+"¿Tenes algún vicio? ¿Cuál?","¿Helado o pizza?","Si pudieras elegir en este momento, ¿con quién estarías, en dónde y qué haciendo?",
+"¿Tenés algún sueño que quieras hacer?","¿Qué es lo que más necesitas en este momento?","¿Como viene tu día hasta ahora? Enserio",
+"¿Cuál fue tu peor pesadilla?","¿Algo tuyo que te cueste admitir?","¿Qué es el amor para vos?","¿En que pensas antes de dormir?","¿Pasta o pollo?",
+"¿Volverías a repetir tu último beso?","¿Preferís navidad o año nuevo?","¿Destino o casualidad?",
+"¿Que te siga y no te de like o que no te siga pero te ponga like?","¿El tiempo te asusta?","¿A quién llamas cuando estás mal?",
+"¿Alguna cosa que no soportes?","Si te encontras un millon de dólares en el piso,¿A quién se lo contás primero?",
+"¿Podrías tener una relación abierta?","¿Cuál es TU dato curioso?","¿Sentís que encontraste la vocación de tu vida?",
+"Pensa en alguien a quien admires, ¿por qué elegiste a esa persona?","¿Algo de lo que estes más agradecido en este momento?",
+"¿Morir pobre o morir virgen?","¿Qué es lo más lindo que te pueden decir?","¿Cómo es el teorema de pitágoras?",
+"¿Alguna vez te perdiste de intentar algo con alguien por miedo?","¿Algo que te salga muy mal o no sepas hacer?",
+"¿Cual es LA cosa tuya que nunca cambiarías de vos?","¿Algún sueño tuyo que hayas abandonado?","¿Te arrepentís de algo?",
+"¿Conconsideras que sabes amar?","¿Cuál es tu mejor plan?","¿Tu color favorito?","¿Cuál fue el mejor chamullo que te hicieron?",
+"Si pudieras vivir por un día en la vida de otra persona, ¿en la de quién?","¿Con que personaje de Disney te sentís identificado?",
+"¿Cuál fue o cuál es la mejor forma de usar tu tiempo?","¿Cuál sería la mejor propuesta de casamiento?","¿El mejor lugar para una primera cita?",
+"¿Qué NO comerías en una primera cita?","¿Cómo te describís en 3 palabras?","¿Cuál sería un motivo para descorchar el champagne?",
+"¿Amar o ser amado?","¿Elegirías saber como o cuando te vas a morir?","Tipo de flor preferida?","¿Qué harías si no tuvieras miedo?",
+"¿Qué queres hacer HOY, que estas dejando para mañana?","¿Alguna vez algún extraño te cambio la vida? ¿Cuándo?",
+"¿Qué estarías necesitando que pase más seguido en tu vida?","¿Qué te hubiese gustado hacer menos?","¿Algo que te haya hecho feliz últimamente?",
+"Si tendrías que definir a la felicidad con una accion ¿qué acción sería?","¿Qué es la libertad para vos?",
+"¿Te convertiste en la persona que querías ser?","¿Alguien que te haya hecho sentir bien últimamente?","¿Qué opinas de la persona de tu derecha?",
+"¿Estás enamorado/a?","¿Que valor aportás a tu entorno?","¿Cómo descargás tus emociones?","Decí una frase o cita que te guste.",
+"¿Hay algo que sea importante para todos y no para vos?"]
+
+//HACER ALGO A PARTIR DE TOCAR UN BOTON
+
+//FORMA 1:
+// const verOferta = () =>{
+//     alert("Estas son las ofertas!")
+// }
+
+
+// // <button class="btn" onclick="verOferta()">Siguiente</button>
+
+
+// //FORMA 2: asignar una función desde js cuando se toca un botón. 
+// let btnOfertas = document.querySelector("#btn-question"); 
+// btnOfertas.onclick = () =>{
+//     alert("Estas son las ofertas!")
+// }
+
+let questionBtn = document.querySelector("#question-btn"); 
+questionBtn.onclick = (list) => { 
+    let questionNumber = document.querySelector("#question-number");
+    let questionBox = document.querySelector("#question-box")
+    list = array
+    let i = Math.random()*list.length
+    i = Math.floor(i)
+    console.log(i)
+    question = list[i]
+    questionBox.textContent = question
+    questionNumber.textContent = `Pregunta número ${i}`
+}
+
+
